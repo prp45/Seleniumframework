85484768b43e6e90b512662ef2fbe5fbeac9683e
RAJ <RAJ@DESKTOP-T9TA374>
2023-03-11 12:23:52 +0530

    1st commit

---
+++ MavenMarch1/src/test/java/Practice/Parameters.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Parameters {
+	@Test
+	public void m1()
+	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+	}
+
+}
---
+++ MavenMarch1/src/test/java/Practice/Sample.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void m1()
+	{
+		System.out.println("m1 running");
+		
+	}
+
+		
+
+
+}
